d986c567ca52782490d31dcee9eca837ac3dd7e5
Krenoa <mtpgadmt@gmail.com>
2020-11-29 22:00:04 +0900

    .patt tukewasureteta tehepero

--- functions/model/image_devil.js
+++ functions/model/image_devil.js
@@ -22,7 +22,7 @@ exports.create_image = async (uid) => {
   console.log(`patternFileString => ${patternFileString}`);
 
   // TODO: saoでpattファイルをストレージに保存する
-  sao.upload_patt('fumo', patternFileString)
+  sao.upload_patt('fumo.patt', patternFileString)
 
   // TODO: imageを返す
 
